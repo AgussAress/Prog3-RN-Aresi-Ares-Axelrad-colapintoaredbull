c3d9f8bb68c798224ba490e2e762d0640aade872
MaximaAresi <maresilavalle@udesa.edu.ar>
2024-11-19 20:53:38 -0300

    primeros cambios del perfil

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import {db} from "../firebase/config"
+import {auth} from "../firebase/config"
 
-export default function Profile() {
+
+export default class Profile extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      userInfo: []
+    }
+  }
+
+  componentDidMount(){
+    db.collection('users')
+    .where("owner", "==", auth.currentUser.email)
+    .onSnapshot((docs)=> {
+      let arrayDocs = []
+
+      docs.forEach((doc)=>{
+        arrayDocs.push({
+          id: doc.id, 
+          data: doc.data()
+        })
+      })
+
+      this.setState({
+        userInfo: arrayDocs
+      }, ()=> console.log("este es el estado", this.state))
+    })
+  }
+
+  render(){
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Profile</Text>
+      {this.state.userInfo.length >0
+      ?
+      (<Text> Nombre de usuario:
+        {this.state.userInfo[0].data.username}
+      </Text>
+      )
+      :
+      (<Text>Cargando informacion...</Text>)}
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
